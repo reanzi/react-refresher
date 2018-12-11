71e42003207f557105e16340d7d18ef824e2a4f9
reanzi <nziku99@gmail.com>
2018-12-11 17:31:03 +0300

    staring, done lesson

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Ninjas extends Component {
+  render() {
+    return (
+      <div className="ninja">
+        <div>Name: Rahel </div>
+        <div>Belt: Black </div>
+      </div>
+    );
+  }
+}
+
+export default Ninjas;
