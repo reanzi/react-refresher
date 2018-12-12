24d951cb8949ae41fb45c327c4cd162a16340cb3
reanzi <nziku99@gmail.com>
2018-12-12 08:58:08 +0300

    conditional outputing something

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,16 +3,34 @@ import React from "react";
 const Ninjas = ({ ninjas }) => {
   // in function comp, props is passed a parameter, accessed without 'this'
   // const { ninjas } = props;
+  /*
   const ninjaList = ninjas.map(ninja => {
-    return (
+    if (ninja.age > 20) {
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div>My name: {ninja.name}</div>
+          <div>My age: {ninja.age}</div>
+          <div>Belt: {ninja.belt} </div>
+          <hr />
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }); 
+  */
+  //  Conditional outiputing using turnali operator
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age < 20 ? (
       <div className="ninja" key={ninja.id}>
         <div>My name: {ninja.name}</div>
         <div>My age: {ninja.age}</div>
         <div>Belt: {ninja.belt} </div>
         <hr />
       </div>
-    );
+    ) : null;
   });
+
   return <div className="ninja-list">{ninjaList}</div>;
 };
 
