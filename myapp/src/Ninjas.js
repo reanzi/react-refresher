cb0b0d1d0a2a6b3d6775f319420f99df3e9936d4
reanzi <nziku99@gmail.com>
2018-12-12 08:48:14 +0300

    Changing class comp into functional comp

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render(props) {
-    // no need to pass props into render as it's given in class base components
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>My name: {ninja.name}</div>
-          <div>My age: {ninja.age}</div>
-          <div>Belt: {ninja.belt} </div>
-          <hr />
-        </div>
-      );
-    });
-    return <div className="ninja-list">{ninjaList}</div>;
-  }
-}
+const Ninjas = ({ ninjas }) => {
+  // in function comp, props is passed a parameter, accessed without 'this'
+  // const { ninjas } = props;
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div className="ninja" key={ninja.id}>
+        <div>My name: {ninja.name}</div>
+        <div>My age: {ninja.age}</div>
+        <div>Belt: {ninja.belt} </div>
+        <hr />
+      </div>
+    );
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
