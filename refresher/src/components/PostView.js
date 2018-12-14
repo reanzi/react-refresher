c5a52fad3fa8163c996d895bd2868d2d63746278
reanzi <nziku99@gmail.com>
2018-12-14 12:59:03 +0300

    Using params to direct user to the post

--- refresher/src/components/PostView.js
+++ refresher/src/components/PostView.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export default class PostView extends Component {
   state = {
-    id: null
+    post: null
   };
   componentDidMount() {
     // console.log(this.props);
     let id = this.props.match.params.post_id;
-    this.setState({
-      id
+    Axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
+      //   const { post } = res.data;
+      // console.log(res.data);
+      this.setState({
+        post: res.data
+      });
+      console.log(this.state);
     });
-    // console.log(id);
   }
   render() {
-    return (
-      <div className="container">
-        <h4>Router Param {this.state.id}</h4>
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p> {this.state.post.body} </p>
       </div>
+    ) : (
+      <div className="center">Loading Posts......</div>
     );
+    return <div className="container">{post}</div>;
   }
 }
