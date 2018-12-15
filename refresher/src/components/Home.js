228ace116b51697e7abc176e364dceb396d6ef53
reanzi <nziku99@gmail.com>
2018-12-15 14:56:32 +0300

    Importing image in React Components

--- refresher/src/components/Home.js
+++ refresher/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Flower from "../roses.jpeg";
 
 class Home extends Component {
   state = {
@@ -20,9 +21,10 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Flower} alt="sunflower with butterfly" />
             <div className="card-content">
               <Link to={"/" + post.id}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title purple-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -33,7 +35,7 @@ class Home extends Component {
       <div className="center"> Loading, Please Wait....</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
