b08de24e3468aece6c4ebc2815e8a3137f01638b
reanzi <nziku99@gmail.com>
2018-12-16 09:16:10 +0300

    Action Creator

--- refresher/src/components/PostView.js
+++ refresher/src/components/PostView.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { deletePost } from "../actions/postActions";
 
 class PostView extends Component {
   handleClick = () => {
@@ -33,12 +34,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    deletePost: id => {
-      dispatch({
-        type: "DELETE_POST",
-        id: id
-      });
-    }
+    deletePost: id => dispatch(deletePost(id))
   };
 };
 export default connect(
