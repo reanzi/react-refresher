26fa361a4f5e5abfc24a827cfa278354f0dda648
reanzi <nziku99@gmail.com>
2018-12-16 08:38:47 +0300

    Display single post from the store

--- refresher/src/components/PostView.js
+++ refresher/src/components/PostView.js
@@ -1,27 +1,12 @@
 import React, { Component } from "react";
-import Axios from "axios";
+import { connect } from "react-redux";
 
-export default class PostView extends Component {
-  state = {
-    post: null
-  };
-  componentDidMount() {
-    // console.log(this.props);
-    let id = this.props.match.params.post_id;
-    Axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
-      //   const { post } = res.data;
-      // console.log(res.data);
-      this.setState({
-        post: res.data
-      });
-      console.log(this.state);
-    });
-  }
+class PostView extends Component {
   render() {
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p> {this.state.post.body} </p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p> {this.props.post.body} </p>
       </div>
     ) : (
       <div className="center">Loading Posts......</div>
@@ -29,3 +14,11 @@ export default class PostView extends Component {
     return <div className="container">{post}</div>;
   }
 }
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+
+  return {
+    post: state.posts.find(post => post.id === id)
+  };
+};
+export default connect(mapStateToProps)(PostView);
